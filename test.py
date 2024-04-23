545f56c774180648d4808f1e11c2551ef304dffa
Nate Kimball <tma5gv@virginia.edu>
2024-04-23 01:07:50 -0400

    got loss fn working, need to select correct token from output

--- test.py
+++ test.py
@@ -17,7 +17,7 @@ mnist = datasets.MNIST(f'/scratch/{getuser()}/datasets/mnist', train=True, downl
 example = mnist[0][0]
 example.save('example.png')
 
-prompt = "USER: <image>\nWhat digit [0-9] is this?\nASSISTANT:"
+prompt = "USER: <image>\nWhat digit [0-9] is this?\nASSISTANT: This is the digit "
 target = "USER: <image>\nWhat digit [0-9] is this?\nASSISTANT: This is the digit 0."
 
 model = LlavaForConditionalGeneration.from_pretrained(
@@ -47,11 +47,18 @@ print(processor.decode(output[0][2:], skip_special_tokens=True))
 target_string = "0"
 target = processor(target_string, return_tensors="pt")
 
-target_ids = target['input_ids']
+target_id = target['input_ids'][0,-1]
 
+s = input_ids.shape[1]+1
+#need to figure this out
+digit_logits = output_logits[:,s]
+digit = processor.decode(torch.argmax(digit_logits))
+print("decoded digit:", digit)
 
 loss_fn = torch.nn.CrossEntropyLoss()
-loss = loss_fn(output_logits.view(-1, output_logits.size(-1)), target_ids.view(-1))
+#loss = loss_fn(output_logits.view(-1, output_logits.size(-1)), target_ids.view(-1))
+print(digit_logits.shape)
+loss = loss_fn(digit_logits, target_id.view(-1).to(digit_logits.device))
 print(loss)
 
 #loss = outputs.loss
